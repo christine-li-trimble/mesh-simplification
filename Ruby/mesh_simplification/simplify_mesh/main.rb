07d1463c0af12bfaaa048321e76b96751720e6e0
Christine Li <christine_li@trimble.com>
2024-06-28 12:37:12 -0700

    Adding the counting of the faces and edges of the selected entity

--- Ruby/mesh_simplification/simplify_mesh/main.rb
+++ Ruby/mesh_simplification/simplify_mesh/main.rb
@@ -5,8 +5,29 @@ require 'sketchup.rb'
 
 module Examples
 
-    def self.create_cube
+    def self.readentity(entity)
+      #puts entity.typename
+      if entity.is_a? Sketchup::Group 
+        puts entity.typename
+        entity.entities.each { |subentity|
+          self.readentity(subentity)
+        }
+      elsif entity.is_a? Sketchup::ComponentInstance
+        puts entity.typename
+        definition = entity.definition
+        definition.entities.each { |subentity|
+           readentity(subentity)
+        }
+      elsif entity.is_a? Sketchup::Face
+        $face_count += 1
+      elsif entity.is_a? Sketchup::Edge
+        $edge_count += 1
+      end
+    end
+
+    def self.readmesh
       model = Sketchup.active_model
+      
       model.start_operation('Create Cube', true)
       group = model.active_entities.add_group
       entities = group.entities
@@ -19,13 +40,31 @@ module Examples
       face = entities.add_face(points)
       face.pushpull(-1.m)
       model.commit_operation
+
+      selection = Sketchup.active_model.selection
+      $face_count = 0
+      $edge_count = 0
+      
+      # Look at all of the entities in the selection.
+      selection.each { |entity|
+        # puts entity.typename
+        self.readentity(entity)   
+      }
+      
+      UI.messagebox("There are " + $face_count.to_s + " faces selected." +
+                    "There are " + $edge_count.to_s + " edges selected.")
+      
+      # mesh = face.mesh
+      # puts mesh.count_polygons
+      # puts mesh.count_points
+
       puts SUEX_MeshSimplification::hello_world();
     end
 
     unless file_loaded?(__FILE__)
       menu = UI.menu('Plugins')
       menu.add_item('01 Mesh simplification') {
-        self.create_cube
+        self.readmesh
       }
       file_loaded(__FILE__)
     end
