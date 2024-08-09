ea2675e08425b3996067094080c429230a125eeb
Di press <63003427+di-press@users.noreply.github.com>
2024-08-09 15:23:03 -0300

    add halfedge draft
    Code version showed during meeting

--- Mesh Simplification/src/SUEX_MeshSimplification.cpp
+++ Mesh Simplification/src/SUEX_MeshSimplification.cpp
@@ -13,6 +13,26 @@ VALUE hello_world() {
 	return GetRubyInterface("Hello World!");
 }
 
+
+class Halfedge
+{
+public:
+	int startVertex;
+	int finalVertex;
+	int face;
+	bool isBoundary;
+
+	int next;
+	int twin;
+
+	Halfedge(int startV, int finalV, int f, bool bound, int n, int t = -1): 
+		startVertex{startVert}, finalVertex{finalV}, face{f}, isBoundary{bound}, next{n}, twin{t}
+	{}
+
+	//void setNext(int n) { next = n; }
+	void setTwin(int t) { twin = t; }
+};
+
 VALUE take_input(void* input) {
 	SUModelRef model = SU_INVALID;
 	// Get a reference to the model in the currently running SketchUp.
@@ -45,6 +65,10 @@ VALUE take_input(void* input) {
 	// Create an array of faces. Each entry will be an array of vertices
 	VALUE ruby_new_faces = rb_ary_new_capa(static_cast<long>(faces.size()));
 
+	std::vector<Halfedge> halfedges;
+
+	int faceIndex = 0;
+
 	for (auto face : faces) {
 		std::vector<SUVertexRef> vertices(3);
 		size_t count_verts = 0;
@@ -64,10 +88,20 @@ VALUE take_input(void* input) {
 			SUEntityToRuby(entity, &ruby_vertex);
 			rb_ary_push(ruby_vertices, ruby_vertex);
 		}
+		
+		// How to get v0. v1 and v2? Dict from SUVertexRef to global index?
+		int localOffset = halfedges.size();
+		Halfedge he(v0, v1, faceIndex, false, localOffset + 1);
+		Halfedge he(v1, v2, faceIndex, false, localOffset + 2);
+		Halfedge he(v2, v0, faceIndex, false, localOffset + 0);
+		halfedges.push_back(he);
 
+		++faceIndex;
+		// this shouldn't be here...
 		rb_ary_push(ruby_new_faces, ruby_vertices);
 	}
 
+	// TODO: create twins and borders...
 
 	// Send out the array of arrays of vertices after doing geometry processing.
 	return ruby_new_faces;
